63bc9a72da4ac8ebf614d4286632fadc91b0a992
bertyn <bertynboulikou@gmail.com>
2020-04-10 10:36:49 +0200

    modif

--- Exercices/exo4.c
+++ Exercices/exo4.c
@@ -17,5 +17,5 @@ int main()
     point p2 = {
         .x = 0,
         .y = 5};
-    printf("la distance euclidien entre p1(%f,%f) et p2(%f,%f)  ets de %f\n", p1.x, p1.y, p2.x, p2.y, distEuclid(p1, p2));
+    printf("la distance euclidien entre p1(%f,%f) et p2(%f,%f)  est de %f\n", p1.x, p1.y, p2.x, p2.y, distEuclid(p1, p2));
 }
