7b62251ff7eadb4470432429068a3bab8f7cf748
bertyn <bertynboulikou@gmail.com>
2020-04-10 10:36:04 +0200

    exo 4 fini

--- Exercices/exo4.c
+++ Exercices/exo4.c
@@ -1,2 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include "exo4.h"
+
+float distEuclid(point p1, point p2)
+{
+    float distance = sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+    return distance;
+}
+
+int main()
+{
+    point p1 = {
+        .x = 0,
+        .y = 0};
+    point p2 = {
+        .x = 0,
+        .y = 5};
+    printf("la distance euclidien entre p1(%f,%f) et p2(%f,%f)  ets de %f\n", p1.x, p1.y, p2.x, p2.y, distEuclid(p1, p2));
+}
---
+++ Exercices/exo4.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <math.h>
+typedef struct
+{
+    float x;
+    float y;
+} point;
