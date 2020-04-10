b237e76e51738cd9cbf23ef50ee6c90e7b1de578
bertyn <bertynboulikou@gmail.com>
2020-04-10 10:38:10 +0200

    ajout ex 6,7,8

--- Exercices/exo5.c
+++ Exercices/exo5.c
@@ -1,2 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+float minArray(float *array, int arrayLength)
+{
+    int i;
+    float total;
+
+    for (i = 0; i < arrayLength; i++)
+    {
+        total += array[i];
+    }
+    return total;
+}
+
+int main(int argc, char const *argv[])
+{
+    float array[4] = {1, 2, 3.5, 4};
+
+    printf("total du tableau %f \n", summArray(array, 4));
+}
