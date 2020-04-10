93c549b8a43b4aefd98e314938d47f3370602800
bertyn <bertynboulikou@gmail.com>
2020-04-10 09:06:32 +0200

    creation exo

---
+++ Exercices/exo1.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float array[4] = { 1,
+                       2,
+                       3,
+                       4 }
+}
--- Exercices/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    int a = 5;
-    int *b;
-    printf("%i", b);
-}
